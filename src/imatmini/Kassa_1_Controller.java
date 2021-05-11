da0544bf0051ace75e232e93da0c3da72e5e687b
Elin <nileli@student.chalmers.se>
2021-05-11 11:02:42 +0200

    fx:id till checkout sida 1

--- src/imatmini/Kassa_1_Controller.java
+++ src/imatmini/Kassa_1_Controller.java
@@ -2,11 +2,19 @@ package imatmini;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 import java.io.IOException;
 
 public class Kassa_1_Controller {
 
+    @FXML private Button backToStoreButton;
+    @FXML private ImageView nextStepImageView;
+    @FXML private Label momsLabel;
+    @FXML private Label antalVarorLabel;
+    @FXML private Label totalPrizeLabel;
     // For switching scenes
     SceneController sceneController = new SceneController();
 
