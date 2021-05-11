196da88c7e109eaeab379ffeb902c25a0157f08b
Elin <nileli@student.chalmers.se>
2021-05-11 11:19:09 +0200

    Sida 1 i checkout, grunden klart (ej funktionalitet)

--- src/imatmini/Kassa_1_Controller.java
+++ src/imatmini/Kassa_1_Controller.java
@@ -5,8 +5,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import se.chalmers.cse.dat216.project.ShoppingItem;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
 
 public class Kassa_1_Controller {
 
@@ -15,12 +20,17 @@ public class Kassa_1_Controller {
     @FXML private Label momsLabel;
     @FXML private Label antalVarorLabel;
     @FXML private Label totalPrizeLabel;
+    @FXML private FlowPane checkoutVarukorgFlowPane;
+
     // For switching scenes
     SceneController sceneController = new SceneController();
+
 
     @FXML
     private void switchAffaren(ActionEvent event) throws IOException {
         sceneController.switchToAffaren(event);
     }
+
+
 
 }
