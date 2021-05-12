fda3b945436c2aaa659aebb1e35b05096e2f10ac
Emelie Edberg <familjen.emelie@gmail.com>
2021-05-12 15:07:11 +0200

    knappr på KassaVarukorgsItem

--- src/imatmini/KassaVarukorgsItem.java
+++ src/imatmini/KassaVarukorgsItem.java
@@ -31,14 +31,15 @@ public class KassaVarukorgsItem extends AnchorPane {
 
 
     private Model model = Model.getInstance();
+    Kassa_1_Controller kassa_1_controller;
 
     private ShoppingItem shoppingItem;
     private StringBuilder unit = new StringBuilder();
     private final static double kImageWidth = 100.0;
     private final static double kImageRatio = 0.75;
 
-    public KassaVarukorgsItem(ShoppingItem shoppingItem) {
-        
+    public KassaVarukorgsItem(ShoppingItem shoppingItem, Kassa_1_Controller kassa_1_controller) {
+        this.kassa_1_controller = kassa_1_controller;
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("KassaVarukorgsItem.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
@@ -69,6 +70,11 @@ public class KassaVarukorgsItem extends AnchorPane {
          */
     }
 
+    @FXML
+    private void merInfo(){
+        kassa_1_controller.openDetailView();
+    }
+
     @FXML private void typeAmount(){
         String newValue = amountTextField.getText();
         newValue = newValue.replaceAll("[^0-9]","");
@@ -77,6 +83,14 @@ public class KassaVarukorgsItem extends AnchorPane {
         }
     }
 
+    @FXML private void tabort(){
+        model.getCardMap().get(shoppingItem.getProduct().getName()).plusMinusPane.toBack();
+        model.getShoppingCart().removeItem(shoppingItem);
+        shoppingItem.setAmount(0);
+        kassa_1_controller.updateKassaVarukorg(model.getShoppingCart().getItems());
+    }
+
+
     private void updateAmount(int amount){
         shoppingItem.setAmount(amount);
 
@@ -84,12 +98,14 @@ public class KassaVarukorgsItem extends AnchorPane {
             model.getCardMap().get(shoppingItem.getProduct().getName()).plusMinusPane.toBack();
             model.getShoppingCart().removeItem(shoppingItem);
         }
-
+        amountTextField.setText( (int)shoppingItem.getAmount() + " " + unit);
+        summaLabel.setText(String.format("%.2f", (shoppingItem.getProduct().getPrice()) * shoppingItem.getAmount()) + " kr" );
         model.getCardMap().get(shoppingItem.getProduct().getName()).getAmountTextField().setText((int)shoppingItem.getAmount() + " " + unit);
         model.getShoppingCart().fireShoppingCartChanged(shoppingItem, true);
     }
     @FXML private void plus(){
         updateAmount((int)(shoppingItem.getAmount() + 1));
+        System.out.println("plus");
     }
 
     @FXML private void minus(){
