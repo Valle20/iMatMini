0f544e95b2ab4f4daa12cddd29ac7ea1dff7b62e
Luke-png <72192057+Luke-png@users.noreply.github.com>
2021-05-27 13:39:53 +0200

    Finna ändringar

--- src/imatmini/Main.java
+++ src/imatmini/Main.java
@@ -26,6 +26,7 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
+        stage.setTitle("iMat");
     }
 
     /**
